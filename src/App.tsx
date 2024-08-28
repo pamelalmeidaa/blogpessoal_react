e2c31aa718916b89d1e1c8d68415da2cb5778901
Pamela <contatopamela@outlook.com>
2024-08-28 09:08:50 -0300

    Model e Service

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
+import Home from "./pages/home/Home"
+import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login"
 
 function App() {
-  
+  // Código TypeScript
+
   return (
-  
-  <>
-    <BrowserRouter>
-        <Navbar/>
+    // Código TSX => HTML + CSS
+    <>
+      <BrowserRouter>
+        <Navbar />
         <div className="min-h-[80vh]">
-        <Routes>
-          <Route path = "/" element={<Home/>}/>
-          <Route path = "/home" element={<Home/>}/>
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Cadastro />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
         </div>
-        <Footer/>
-    </BrowserRouter>
-  </>
+        <Footer />
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, ReactNode, useState } from "react";
+import UsuarioLogin from "../models/UsuarioLogin";
+import { login } from "../services/service";
+
+interface AuthContextProps {
+  usuario: UsuarioLogin;
+
+  handleLogin(usuario: UsuarioLogin): Promise<void>;
+  handleLogout(): void;
+  isLoading: boolean;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+export const AuthContext = createContext({} as AuthContextProps);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [usuario, setUsuario] = useState<UsuarioLogin>({
+    id: 0,
+    nome: "",
+    usuario: "",
+    senha: "",
+    foto: "",
+    token: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleLogin(usuarioLogin: UsuarioLogin) {
+    setIsLoading(true);
+    try {
+      await login(`/usuarios/logar`, usuarioLogin, setUsuario);
+      alert("Usuário autenticado com sucesso!");
+    } catch (error) {
+      alert("Os dados do usuário estão inconsistentes!");
+    }
+    setIsLoading(false);
+  }
+  function handleLogout() {
+    setUsuario({
+      id: 0,
+      nome: "",
+      usuario: "",
+      senha: "",
+      foto: "",
+      token: "",
+    });
+  }
+  return <AuthContext.Provider value={{ usuario, handleLogin, handleLogout, isLoading }}>{children}</AuthContext.Provider>;
+}
+
+export default AuthContext;
