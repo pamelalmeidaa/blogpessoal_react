78de6414a9e0bdf6f63753c3087353679a790bad
Pamela Salgado <168048799+pamelalmeidaa@users.noreply.github.com>
2024-09-06 08:30:04 -0300

    Deploy do Blog Pessoal

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,30 +5,13 @@ import { ToastAlerta } from "../../utils/ToastAlerta";
 
 function Navbar() {
 
-    /**
-      * Cria a constante navigate, que receberá o Hook useNavigate().
-      * Através da constante navigate, o usuário será redirecionado 
-      * para outras rotas da aplicação, conforme a necessidade.
-      */
+ 
     const navigate = useNavigate()
 
-    /**
-     * Criamos uma desestruturação para receber a função handleLogout(), 
-     * disponível na Context AuthContext, através do Hook useContext(). 
-     */
+
     const { usuario, handleLogout } = useContext(AuthContext)
 
-    /**
-     * Criamos a função logout(), que será responsável por efetuar 
-     * o processo de logout do usuário. A função logout() executa a 
-     * função handleLogout() da Context AuthContext, responsável por 
-     * reiniciar os dados do usuário no contexto da aplicação, ou seja, 
-     * retornando para o Estado inicial. 
-     * 
-     * Na sequência, será exibido um alerta para informar o usuário que 
-     * o logout foi efetuado com sucesso e redireciona o usuário para a 
-     * Página de Login, através da constante navigate. 
-     */
+
     function logout() {
         handleLogout()
         ToastAlerta('O usuário foi desconectado com sucesso!', 'info')
